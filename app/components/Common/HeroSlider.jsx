1c133b0936a6401c1ec7928853b096d22ad8ea7a
Muhammad Salik <salikmohammed00@gmail.com>
2025-02-20 22:40:10 +0400

    push code

--- app/components/Common/HeroSlider.jsx
+++ app/components/Common/HeroSlider.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
 
 import SliderOne from "@/public/sliderone.png";
 import SliderTwo from "@/public/slidertwo.jpg";
@@ -19,15 +18,18 @@ const HeroSlider = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress((prev) => (prev < 100 ? prev + 1 : 0));
+      setProgress((prev) => (prev + 1) % 101);
     }, slideDuration / 100);
 
-    if (progress >= 100) {
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+
+    if (progress === 100) {
       setCurrentSlide((prev) => (prev + 1) % totalSlides);
       setProgress(0);
     }
-
-    return () => clearInterval(interval);
   }, [progress]);
 
   useEffect(() => {
@@ -36,28 +38,17 @@ const HeroSlider = () => {
       const size = window.innerWidth > 768 ? 138 : 115;
       const borderSize = 8;
       const totalLength = size * 4;
-      const progressLength =
-        (overallProgress / (totalSlides * 100)) * totalLength;
+      const progressLength = (overallProgress / (totalSlides * 100)) * totalLength;
 
       let backgroundPosition = "";
       if (progressLength <= size) {
-        backgroundPosition = `${-size + progressLength}px 0px, ${
-          size - borderSize
-        }px -${size}px, ${size}px ${size - borderSize}px, 0px ${
-          size - borderSize
-        }px`;
+        backgroundPosition = `${-size + progressLength}px 0px, ${size - borderSize}px -${size}px, ${size}px ${size - borderSize}px, 0px ${size - borderSize}px`;
       } else if (progressLength <= size * 2) {
-        backgroundPosition = `0px 0px, ${size - borderSize}px ${
-          -size + (progressLength - size)
-        }px, ${size}px ${size - borderSize}px, 0px ${size - borderSize}px`;
+        backgroundPosition = `0px 0px, ${size - borderSize}px ${-size + (progressLength - size)}px, ${size}px ${size - borderSize}px, 0px ${size - borderSize}px`;
       } else if (progressLength <= size * 3) {
-        backgroundPosition = `0px 0px, ${size - borderSize}px 0px, ${
-          size - (progressLength - size * 2)
-        }px ${size - borderSize}px, 0px ${size - borderSize}px`;
+        backgroundPosition = `0px 0px, ${size - borderSize}px 0px, ${size - (progressLength - size * 2)}px ${size - borderSize}px, 0px ${size - borderSize}px`;
       } else {
-        backgroundPosition = `0px 0px, ${size - borderSize}px 0px, 0px ${
-          size - borderSize
-        }px, 0px ${size - (progressLength - size * 3)}px`;
+        backgroundPosition = `0px 0px, ${size - borderSize}px 0px, 0px ${size - borderSize}px, 0px ${size - (progressLength - size * 3)}px`;
       }
 
       overallProgressRef.current.style.backgroundPosition = backgroundPosition;
@@ -69,55 +60,57 @@ const HeroSlider = () => {
     setProgress(0);
 
     if (overallProgressRef.current) {
-      overallProgressRef.current.style.backgroundPosition =
-        "0px 0px, 130px 0px, 0px 130px, 0px 0px";
+      overallProgressRef.current.style.backgroundPosition = "0px 0px, 130px 0px, 0px 130px, 0px 0px";
     }
   };
 
+  const [textContainerClass, setTextContainerClass] = useState("text-container");
+
+  useEffect(() => {
+    setTextContainerClass("text-container");
+
+
+    const timeout = setTimeout(() => {
+      setTextContainerClass("text-container show");
+    }, 10);
+
+    return () => clearTimeout(timeout);
+  }, [currentSlide]);
+
   return (
     <div className="h-screen flex justify-center relative overflow-hidden">
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={currentSlide}
-          initial={{ scale: 1.1, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          exit={{ opacity: 0, scale: 1 }}
-          transition={{ duration: 1, ease: "easeOut" }}
-          className="absolute inset-0 w-full h-full z-10"
-        >
-          <Image
-            src={slides[currentSlide]}
-            alt="slider"
-            layout="fill"
-            objectFit="cover"
-            className="w-full h-full"
-          />
-        </motion.div>
-      </AnimatePresence>
+      <div className="absolute inset-0 w-full h-full z-10">
+        <Image
+          src={slides[currentSlide]}
+          alt="slider"
+          layout="fill"
+          objectFit="cover"
+          className="w-full h-full"
+        />
+      </div>
 
       <div className="relative md:left-[135px] left-4 z-10 flex flex-col text-left justify-center items-start w-full max-w-[1440px]">
-        <span className="md:text-secondary text-[#F9F4EE] text-sm md:text-[16px] leading-[24px]">
-          Welcome To TenTwenty Farms
-        </span>
-        <h1 className="md:text-xxl text-[46px] text-secondary md:leading-[64px] leading-[46px] mt-[15px] md:mt-[48px] capitalize font-normal">
-          From Our <br className="block md:hidden" /> Farms <br className="" />{" "}
-          to your hands
-        </h1>
+        <div key={currentSlide} className={`${textContainerClass} flex flex-col`}>
+          <span className="md:text-secondary text-[#F9F4EE] text-sm md:text-[16px] leading-[24px]">
+            Welcome To TenTwenty Farms
+          </span>
+          <h1 className="md:text-xxl text-[46px] text-secondary md:leading-[64px] leading-[46px] mt-[15px] md:mt-[48px] capitalize font-normal">
+            From Our <br className="block md:hidden" /> Farms <br className="" />
+            to your hands
+          </h1>
+        </div>
 
         <div className="absolute bottom-[100px] md:left-[168px] left-[138px] flex items-center gap-4">
-          <span className="md:text-secondary text-[#F9F4EE] md:text-[16px] text-sm">
+          <span className="md:text-secondary text-[#F9F4EE] text-sm md:text-[16px] leading-[24px]">
             {String(currentSlide + 1).padStart(2, "0")}
           </span>
           <div
             className="md:w-[103px] w-[100px] h-[1px] overflow-hidden relative"
             style={{ backgroundColor: "rgba(238, 244, 249, 0.3)" }}
           >
-            <motion.div
-              key={currentSlide}
-              className="h-full bg-white absolute left-0 top-0"
-            />
+            <div className="h-full bg-white absolute left-0 top-0" />
           </div>
-          <span className="md:text-secondary text-[#F9F4EE] md:text-[16px] text-sm">
+          <span className="md:text-secondary text-[#F9F4EE] text-sm md:text-[16px] leading-[24px]">
             0{totalSlides}
           </span>
         </div>
